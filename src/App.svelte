427fe84565b1d7748f03ff8deac188c81ad215b2
kjj6198 <kjj6198@gmail.com>
2020-10-25 16:20:56 +0900

    reimplement using DOM

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,32 @@
 <script>
-  import { onMount } from 'svelte';
+  import { onMount, onDestroy } from 'svelte';
   import AudioAnalyser from './audio/AudioAnalyser';
   import SpectrumCanvas from './audio/SpectrumCanvas';
 
   let audio;
   let canvas;
-  onMount(() => {
-    const analyser = new AudioAnalyser(audio);
-    analyser.connect();
-    audio.addEventListener('play', () => {
-      analyser.process();
-    });
-    audio.addEventListener('pause', analyser.stop);
-    console.log(analyser.totalVolume);
-    const drawer = new SpectrumCanvas(canvas.getContext('2d'), 105);
+  let analyser;
+  let spectrum = [];
+  let userPerformAction = false;
 
-    analyser.store.subscribe((spectrum) => {
-      console.log(spectrum);
-      drawer.draw(spectrum);
-    });
-  });
+  $: {
+    if (audio && userPerformAction) {
+      analyser = new AudioAnalyser(audio);
+      analyser.connect();
+
+      analyser.process();
+
+      audio.addEventListener('pause', analyser.stop);
+    }
+  }
+
+  $: {
+    if (analyser) {
+      analyser.store.subscribe((volume) => {
+        spectrum = volume;
+      });
+    }
+  }
 
   export let name;
 </script>
@@ -28,7 +35,6 @@
   main {
     text-align: center;
     padding: 1em;
-    max-width: 240px;
     margin: 0 auto;
   }
 
@@ -50,9 +56,45 @@
     width: 500px;
     height: 500px;
   }
+
+  .spectrum {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    display: inline-block;
+    width: 3px;
+    height: 500px;
+    background-color: #27cc95;
+    margin-right: 2px;
+    transition: transform ease 0.2s;
+    transform: scale(0.01);
+    transform-origin: center bottom;
+  }
+
+  .container {
+    position: relative;
+    height: 500px;
+    width: 100%;
+    margin: 0 auto;
+  }
 </style>
 
 <main>
-  <audio bind:this={audio} src="music.mp3" controls />
-  <canvas bind:this={canvas} width="1000" height="1000" />
+  <audio
+    on:play={() => {
+      userPerformAction = true;
+      analyser.process();
+    }}
+    bind:this={audio}
+    src="music.mp3"
+    controls />
+  <div class="container">
+    {#each spectrum as vol, i}
+      <span
+        class="spectrum"
+        style="transform: scale(1, {vol / 255 + 0.005}); left: {i * 5}px;
+        background-color: hsl({(180 / Math.PI) * ((i / spectrum.length) * 360 * (Math.PI / 180))},
+        100%, 50%)" />
+    {/each}
+  </div>
 </main>
